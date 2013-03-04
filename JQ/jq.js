fb51ca723fb9d3118f651ce5fdd49a51f36d246d
BiiiG <kamyab@hooshmand.in>
2013-03-04 15:53:54 +0330

    JQ Training

---
+++ JQ/jq.js
@@ -0,0 +1,3 @@
+$(".box > div.x").html("BiiiG Design").css('color','darkred').css('font-size','30px').css('font-family','arial').css('text-align','center').mouseleave(function(){
+$(".box > div.x").css('font-size','25px').html('BiiiG ha dar hale paridan az ghafasand').slideUp(5000);
+alert('BiiiG az ghafas parid');});
