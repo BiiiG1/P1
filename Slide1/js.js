a6a1c2c2c598dbb4682530ccd4efcd2301a4e0a1
BiiiG <kamyab@hooshmand.in>
2013-03-06 23:57:32 +0330

    SlideShow Added

---
+++ Slide1/js.js
@@ -0,0 +1,16 @@
+$("div.slide1p").click(function(){
+	$("div.slide").css('background','url(1.jpg)');
+});
+$("div.slide2p").click(function(){
+	$("div.slide").css('background','url(2.jpg)');
+});
+$("div.slide3p").click(function(){
+	$("div.slide").css('background','url(3.jpg)');
+});
+$("div.slide4p").click(function(){
+	$("div.slide").css('background','url(4.jpg)');
+});
+$("div.slide5p").click(function(){
+	$("div.slide").css('background','url(5.jpg)');
+});
+
